0bf8a215e536f7c07b8a6d024e2c8a2aa003e043
Jorg <jorg@iambnb.nl>
2020-01-09 20:59:04 +0100

    added code from REPL.IT

---
+++ lesson 2/code.js
@@ -0,0 +1,78 @@
+// // Boolean
+// // && AND
+// // || OR
+// // ! NOT
+
+// // ternary operator ?
+//   glassEmpty || glassPretty ? functionWhenTrue() : functionWhenFalse();
+
+// let glassEmpty = false
+// let plateEmpty = true
+
+// let thirsty = null 
+
+// // if (glassEmpty()) {
+
+
+// //   console.log('fill glass')
+// // } else if(plateEmpty) {
+// //   console.log('get more food')
+// // } else if () {
+
+// // }
+
+// let status = null;
+
+// console.log(status)
+
+// let car = 'Mercedes'
+
+// car[0] = 'm';
+
+// car = "mercedes"
+
+// console.log(car.slice(0, 3))
+
+// console.log(car)
+
+let input = 'c'
+
+switch(input) {
+  case 'a':
+    console.log('it\'s a');
+
+  case 'b':
+    console.log('it\'s b');
+    break;
+
+  case 'c':
+    console.log('it\'s c');
+
+  default:
+    console.log('it\'s none of the above')
+
+}
+
+let arr = [];
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i)
+// }
+
+let i = 5
+
+// while(i < 10) {
+//   console.log(i)
+//   i++
+// }
+
+do {
+  i++
+  console.log(i)
+} while (i < 5)
+
+console.log(i)
+
+while(true) {
+ console.log('code');
+}
